cbc98b3bb71a03374d6f115644723a01098911a8
Pravintobi <a.pravin1922@gmail.com>
2024-11-20 22:13:31 +0530

    empyt

--- nodejs-master/modules/mentor.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const mentorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  expertise: { type: String, required: true },
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
-});
-
-export default mongoose.model('Mentor', mentorSchema);
